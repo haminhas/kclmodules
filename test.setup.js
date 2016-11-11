054bd3d614e46cb1f64b63ff9aad6e758bcd9d05
Hassan Minhas <haminhas@live.com>
2016-11-11 20:44:32 +0000

    test setup

---
+++ test.setup.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import 'babel-polyfill';
+import { jsdom } from 'jsdom';
+import chai from 'chai';
+import chaiGen from 'chai-generator';
+import chaiAsPromised from 'chai-as-promised';
+import dirtyChai from 'dirty-chai';
+import sinon from 'sinon';
+import sinonAsPromised from 'sinon-as-promised';
+import sinonChai from 'sinon-chai';
+import hook from 'css-modules-require-hook';
+
+
+const noop = () => (null);
+
+require.extensions['.svgi'] = noop;
+require.extensions['.svg'] = noop;
+require.extensions['.png'] = noop;
+
+hook({
+  generateScopedName: '[name]__[local]___[hash:base64:5]',
+});
+
+global.document = jsdom('');
+global.window = global.document.defaultView;
+
+chai.use(chaiGen);
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+chai.use(dirtyChai);
+global.expect = chai.expect;
+global.sinon = sinon;
+global.navigator = {
+  userAgent: 'node.js',
+};
