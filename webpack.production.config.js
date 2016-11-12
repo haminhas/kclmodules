4e69eb4253052b7568ccec780eb08798123db908
Hassan Minhas <haminhas@live.com>
2016-11-12 23:56:10 +0000

    chnages to start script

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
-    filename: '[name]-[hash].min.js',
+    filename: 'index.js',
     publicPath: '/'
   },
   module: {
