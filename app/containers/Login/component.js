f1fe71c3c7f1012e785bef45dc1b4c2a3255706f
Hassan Minhas <haminhas@live.com>
2016-11-27 21:31:46 +0000

    production fix

--- app/containers/Login/component.js
+++ app/containers/Login/component.js
@@ -3,10 +3,11 @@ import SmallContainer from 'app/components/SmallContainer';
 import { browserHistory } from 'react-router';
 
 import style from './style.css';
-
+const { API_GATEWAY_URL } = process.env;
+const url = API_GATEWAY_URL + '/auth';
 const Login = () => (
   <SmallContainer>
-    <a className={style.button} href="http://localhost:3000/auth">Log In</a>
+    <a className={style.button} href={url}>Log In</a>
     <div onClick={() => browserHistory.push('/dashboard')}>DashBoard</div>
   </SmallContainer>
 );
